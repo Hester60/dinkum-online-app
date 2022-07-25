7ebb3cd8608fc6276b8f6fb35d88f2f23d7c310a
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-25 12:14:57 +0200

    Adding footer

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import MainAppBar from "./components/Navigation/MainAppBar";
-import { Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Catchables from "./containers/catchables/Catchables";
 import Box from "@mui/material/Box";
 import {Container} from "@mui/material";
+import MainFooter from "./components/Footer/MainFooter";
 
 const Routing = () => {
-  return (
-      <Routes>
-        <Route path="/catchables" element={<Catchables />} />
-        <Route path="/" element={<Navigate to="/catchables" replace={true} />} />
-      </Routes>
-  )
+    return (
+        <Routes>
+            <Route path="/catchables" element={<Catchables/>}/>
+            <Route path="/" element={<Navigate to="/catchables" replace={true}/>}/>
+        </Routes>
+    )
 }
 
 const PageLayout = ({children}) => {
-  return (
-      <div>
-        <MainAppBar />
-        <Container>
-          <Box py={3}>
-            {children}
-          </Box>
-        </Container>
-      </div>
-  )
+    return (
+        <div>
+            <MainAppBar/>
+            <Container>
+                <Box py={3}>
+                    {children}
+                </Box>
+            </Container>
+            <MainFooter/>
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <PageLayout>
-      <Routing />
-    </PageLayout>
-  );
+    return (
+        <PageLayout>
+            <Routing/>
+        </PageLayout>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/MainFooter.js
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import {Container} from "@mui/material";
+
+export default function MainFooter() {
+    return (
+        <Box component="div" py={2} bgcolor="primary.main" color="white">
+            <Container sx={{textAlign: 'center'}}>
+                <Typography fontSize="small">
+                    Made by <a className="styled-link" href="https://thomascarbuccia.fr" rel="noreferrer" target="_blank">Thomas CARBUCCIA</a>.
+                    Dinkum Online is a fan-made website that claims no ownership of any intellectual property associated
+                    with James Bendon and Dinkum.
+                </Typography>
+            </Container>
+        </Box>
+    )
+}
