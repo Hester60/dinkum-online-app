3d13b3abb630152d534a33957c1c9c19f1e882be
Hester60 <thomascarbuccia@hotmail.fr>
2022-07-25 14:40:16 +0200

    Custom theme colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import MainAppBar from "./components/Navigation/MainAppBar";
 import {Routes, Route, Navigate} from "react-router-dom";
 import Catchables from "./containers/catchables/Catchables";
 import Box from "@mui/material/Box";
-import {Container} from "@mui/material";
+import {Container, createTheme, ThemeProvider} from "@mui/material";
 import MainFooter from "./components/Footer/MainFooter";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#572e14',
+        },
+        secondary: {
+            main: '#2e7d32',
+        },
+    },
+});
 
 const Routing = () => {
     return (
@@ -30,9 +41,11 @@ const PageLayout = ({children}) => {
 
 function App() {
     return (
-        <PageLayout>
-            <Routing/>
-        </PageLayout>
+        <ThemeProvider theme={theme}>
+            <PageLayout>
+                <Routing/>
+            </PageLayout>
+        </ThemeProvider>
     );
 }
 
